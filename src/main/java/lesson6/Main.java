f41ac253e5ce0b826c9ebc4c6ab4f2991bd2e782
Ilya We4tex <sunask@yandex.ru>
2021-10-09 08:37:11 +0400

    add l6

--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -1,4 +1,60 @@
 package lesson6;
 
+//Урок 6. Работа с сетью
+//1. С помощью http запроса получить в виде json строки погоду в Москве на период времени,
+// пересекающийся со следующим занятием (например, выборка погода на следующие 5 дней - подойдет)
+//2. Подобрать источник самостоятельно. Можно использовать api accuweather, порядок следующий:
+// - зарегистрироваться,
+// - зарегистрировать тестовое приложение для получения api ключа,
+// - найдите нужный endpoint и изучите документацию.
+//P.S.: Бесплатный тарифный план предполагает получение погоды не более чем на 5 дней вперед
+// (этого достаточно для выполнения д/з).
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main {
+
+    // X-Yandex-API-Key: e90d3afa-a29d-4d1b-8a6b-a9206f1cdba1
+
+    private static final String X_Yandex_API_Key = "e90d3afa-a29d-4d1b-8a6b-a9206f1cdba1";
+    private static final String BASE_HOST = "api.weather.yandex.ru";
+    private static final String API_VER="v2";
+    private static final String PATH_SEGMENT = "forecast";
+    private static final String LAT ="59.95";
+    private static final String LON = "30.16";
+    private static final String LIMIT = "5";
+    private static final String LANG = "ru_RU";
+    private static final String EXTRA = "true";
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        // Формируем запрос.
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host(BASE_HOST)
+                .addEncodedPathSegment(API_VER)
+                .addEncodedPathSegment(PATH_SEGMENT)
+                .addQueryParameter("lat", LAT).addQueryParameter("lon", LON)
+                .addQueryParameter("lang", LANG).addQueryParameter("limit", LIMIT)
+                .addQueryParameter("extra", EXTRA)
+                .build();
+
+        System.out.println("Выведем на экран созданный запрос:\n" + url);
+        Request requestHttp = new Request.Builder()
+                .addHeader("X-Yandex-API-Key", X_Yandex_API_Key)
+                .url(url)
+                .build();
+
+        Response response = okHttpClient.newCall(requestHttp).execute();
+        String body = Objects.requireNonNull(okHttpClient.newCall(requestHttp).execute().body()).string();
+
+        System.out.println("Код ответа сервера: " + response.code());
+        System.out.println("JSON строка:\n" + body);
+
+    }
 }
